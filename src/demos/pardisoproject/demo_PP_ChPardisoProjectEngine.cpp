44ce8db01d4fca12ede90e0ce2c9a3b54ad9ce1c
DarioMangoni <dariomangoni@gmail.com>
2021-01-07 18:33:39 +0100

    Modified header for PardisoProject demos

--- src/demos/pardisoproject/demo_PP_ChPardisoProjectEngine.cpp
+++ src/demos/pardisoproject/demo_PP_ChPardisoProjectEngine.cpp
@@ -1,16 +1,16 @@
-/* -------------------------------------------------------------------- */
-/*      Example program to show the use of the "PARDISO" routine        */
-/*      on for unsymmetric linear systems                               */
-/* -------------------------------------------------------------------- */
-/*      This program can be downloaded from the following site:         */
-/*      http://www.pardiso-project.org                                  */
-/*                                                                      */
-/*  (C) Olaf Schenk, Institute of Computational Science                 */
-/*      Universita della Svizzera italiana, Lugano, Switzerland.        */
-/*      Email: olaf.schenk@usi.ch                                       */
-/* -------------------------------------------------------------------- */
-
-// C++ compatible
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2014 projectchrono.org
+// All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: Dario Mangoni
+// =============================================================================
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,25 +21,15 @@
 using namespace std;
 using namespace chrono;
 
-/* PARDISO prototype. */
-extern "C" void pardisoinit (void   *, int    *,   int *, int *, double *, int *);
-extern "C" void pardiso     (void   *, int    *,   int *, int *,    int *, int *, 
-                  double *, int    *,    int *, int *,   int *, int *,
-                     int *, double *, double *, int *, double *);
-extern "C" void pardiso_chkmatrix  (int *, int *, double *, int *, int *, int *);
-extern "C" void pardiso_chkvec     (int *, int *, double *, int *);
-extern "C" void pardiso_printstats (int *, int *, double *, int *, int *, int *, double *, int *);
-
-//#define useCptr
-
 int main( void ) 
 {
-    ChPardisoProjectEngine ppengine(ChPardisoProjectEngine::pardisoproject_SYM::UNSYMMETRIC);
-        /* Matrix data. */
+    ChPardisoProjectEngine ppengine(ChPardisoProjectEngine::parproj_SYM::UNSYMMETRIC);
+    
+    /* Matrix data. */
     int    n = 8;
     int    ia[ 9] = { 0, 4, 7, 9, 11, 12, 15, 17, 20 };
-    int    ja[20] = { 0,    2,       5, 6, 
-                         1, 2,    4,
+    int    ja[20] = { 0,    2,         5,  6, 
+                         1, 2,     4,
                             2,             7,
                                3,       6,
                          1,
@@ -47,52 +37,13 @@ int main( void )
                          1,             6,
                             2,          6, 7 };
     double  a[20] = { 7.0,      1.0,           2.0, 7.0, 
-                    -4.0, 8.0,      2.0,
-                        1.0,                     5.0,
+                     -4.0, 8.0,      2.0,
+                           1.0,                     5.0,
                                 7.0,           9.0,
-                    -4.0,
+                     -4.0,
                         7.0,           3.0,      8.0,
                     1.0,                    11.0,
                         -3.0,                2.0, 5.0 };
-#ifdef useCptr
-
-    int      nnz = ia[n];
-    ppengine.SetMatrix(n, ia, ja, a);
-
-    //int      mtype = 11;        /* Real unsymmetric matrix */
-
-    /* RHS and solution vectors. */
-    double   b[8], x[8];
-
-    /* Set right hand side to one. */
-    for (int i = 0; i < n; i++) {
-        b[i] = 1.0;
-        x[i] = 999.9;
-    }
-
-    ppengine.SetSolutionVector(x);
-    ppengine.SetRhsVector(b);
-
-    ///* -------------------------------------------------------------------- */    
-    ///* ..  Convert matrix from 0-based C-notation to Fortran 1-based        */
-    ///*     notation.                                                        */
-    ///* -------------------------------------------------------------------- */ 
-    //for (int i = 0; i < n+1; i++) {
-    //    ia[i] += 1;
-    //}
-    //for (int i = 0; i < nnz; i++) {
-    //    ja[i] += 1;
-    //}
-
-#else
-    //ChSparseMatrix mmat;
-    //mmat.resize(3,3);
-    //mmat.reserve(3);
-    //mmat.insert(0, 0) = 1.0;
-    //mmat.insert(1, 1) = 1.0;
-    //mmat.insert(2, 2) = 1.0;
-    //mmat.makeCompressed();
-
 
     ChSparseMatrix mmat;
     mmat.resize(8, 8);
@@ -114,15 +65,6 @@ int main( void )
     ppengine.SetMatrix(mmat);
     ppengine.SetSolutionVector(mx);
     ppengine.SetRhsVector(mb);
-
-    
-
-    std::cout << "mmat is:" << std::endl << mmat << std::endl;
-    std::cout << "nnz is:" <<mmat.nonZeros() << std::endl;
-    std::cout << "rhs is:" << std::endl << mb << std::endl;
-    std::cout << "sol is:" << std::endl << mx << std::endl;
-
-#endif
 
     /* -------------------------------------------------------------------- */
     /*  .. pardiso_chk_matrix(...)                                          */
@@ -147,7 +89,7 @@ int main( void )
     /*    Use this functionality only for debugging purposes                */
     /* -------------------------------------------------------------------- */
 
-    ppengine.PrintStats();
+    ppengine.CheckMatrixStats();
     
     /* -------------------------------------------------------------------- */
     /* ..  Reordering and Symbolic Factorization. This step also allocates  */
@@ -155,7 +97,7 @@ int main( void )
     /*     ppengine.PardisoProjectCall                                      */
     /* -------------------------------------------------------------------- */
 
-    ppengine.PardisoProjectCall(ChPardisoProjectEngine::pardisoproject_PHASE::ANALYZE);
+    ppengine.PardisoProjectCall(ChPardisoProjectEngine::parproj_PHASE::ANALYZE);
     
     if (ppengine.GetLastError() != 0) {
         printf("\nERROR during symbolic factorization: %d", ppengine.GetLastError());
@@ -170,7 +112,7 @@ int main( void )
     /* -------------------------------------------------------------------- */
     
     ppengine.SetIPARM(32, 1);
-    ppengine.PardisoProjectCall(ChPardisoProjectEngine::pardisoproject_PHASE::FACTORIZE);
+    ppengine.PardisoProjectCall(ChPardisoProjectEngine::parproj_PHASE::FACTORIZE);
 
     if (ppengine.GetLastError() != 0) {
         printf("\nERROR during numerical factorization: %d", ppengine.GetLastError());
@@ -183,7 +125,7 @@ int main( void )
     /* ..  Back substitution and iterative refinement.                      */
     /* -------------------------------------------------------------------- */
 
-    ppengine.PardisoProjectCall(ChPardisoProjectEngine::pardisoproject_PHASE::SOLVE);
+    ppengine.PardisoProjectCall(ChPardisoProjectEngine::parproj_PHASE::SOLVE);
 
    
     if (ppengine.GetLastError() != 0) {
@@ -195,71 +137,12 @@ int main( void )
 
     ppengine.SetZeroIndexedFormat();
 
+    std::cout << "mmat is:" << std::endl << mmat << std::endl;
+    std::cout << "nnz is:" << mmat.nonZeros() << std::endl;
+    std::cout << "rhs is:" << std::endl << mb << std::endl;
+    std::cout << "sol is:" << std::endl << mx << std::endl;
 
-    #ifdef useCptr
-    #else
-        std::cout << "mmat is:" << std::endl << mmat << std::endl;
-        std::cout << "rhs is:" << std::endl << mb << std::endl;
-        std::cout << "sol is:" << std::endl << mx << std::endl;
-    #endif
-
-
-///* -------------------------------------------------------------------- */
-///* ..  Back substitution with tranposed matrix A^t x=b                   */
-///* -------------------------------------------------------------------- */
-//    phase = 33;
-//
-//    iparm[7]  = 1;       /* Max numbers of iterative refinement steps. */
-//    iparm[11] = 1;       /* Solving with transpose matrix. */
-//
-//    /* Set right hand side to one. */
-//    for (i = 0; i < n; i++) {
-//        b[i] = 1;
-//    }
-//  
-//    pardiso (pt, &maxfct, &mnum, &mtype, &phase,
-//             &n, a, ia, ja, &idum, &nrhs,
-//             iparm, &msglvl, b, x, &error,  dparm);
-//  
-//    if (error != 0) {
-//        printf("\nERROR during solution: %d", error);
-//        exit(3);
-//    }
-//
-//    printf("\nSolve completed ... ");
-//    printf("\nThe solution of the system is: ");
-//    for (i = 0; i < n; i++) {
-//        printf("\n x [%d] = % f", i, x[i] );
-//    }
-//    printf ("\n");
-
-///* -------------------------------------------------------------------- */    
-///* ..  Convert matrix back to 0-based C-notation.                       */
-///* -------------------------------------------------------------------- */ 
-//    for (i = 0; i < n+1; i++) {
-//        ia[i] -= 1;
-//    }
-//    for (i = 0; i < nnz; i++) {
-//        ja[i] -= 1;
-//    }
-
-/* -------------------------------------------------------------------- */    
-/* ..  Termination and release of memory.                               */
-/* -------------------------------------------------------------------- */ 
-
-#ifdef useCptr
-    ///* -------------------------------------------------------------------- */    
-    ///* ..  Convert matrix back to 0-based C-notation.                       */
-    ///* -------------------------------------------------------------------- */ 
-    //for (int i = 0; i < n+1; i++) {
-    //    ia[i] -= 1;
-    //}
-    //for (int i = 0; i < nnz; i++) {
-    //    ja[i] -= 1;
-    //}
-#endif
-
-    ppengine.PardisoProjectCall(ChPardisoProjectEngine::pardisoproject_PHASE::END);
+    ppengine.PardisoProjectCall(ChPardisoProjectEngine::parproj_PHASE::END);
 
     return 0;
 } 
--- src/demos/pardisoproject/demo_PP_PardisoProject.cpp
+++ src/demos/pardisoproject/demo_PP_PardisoProject.cpp
@@ -1,16 +1,16 @@
-/* -------------------------------------------------------------------- */
-/*      Example program to show the use of the "PARDISO" routine        */
-/*      on for unsymmetric linear systems                               */
-/* -------------------------------------------------------------------- */
-/*      This program can be downloaded from the following site:         */
-/*      http://www.pardiso-project.org                                  */
-/*                                                                      */
-/*  (C) Olaf Schenk, Institute of Computational Science                 */
-/*      Universita della Svizzera italiana, Lugano, Switzerland.        */
-/*      Email: olaf.schenk@usi.ch                                       */
-/* -------------------------------------------------------------------- */
-
-// C++ compatible
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2014 projectchrono.org
+// All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: Dario Mangoni
+// =============================================================================
 
 #include <stdio.h>
 #include <stdlib.h>
